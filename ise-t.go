410109595e4f010aa1f97fb8e158eed7d66bc5d9
Steven McNutt <stmcnutt@cisco.com>
2022-07-18 11:11:08 +0000

    struct to parse device list json

--- ise-t.go
+++ ise-t.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -15,13 +16,42 @@ var (
 	certs bool
 )
 
+type NodeList struct {
+	SearchResult struct {
+		Total     int
+		Resources []struct {
+			Id   string
+			Name string
+			Link struct {
+				Rel  string
+				Href string
+				Type string
+			}
+		}
+	}
+}
+
+// environment vars for connecting to ISE
 var ise = make(map[string]string)
 
+// store api endpoints in a map for easy retrieval
+var endPoints = make(map[string]string)
+
 func main() {
+	var nodelist NodeList
 	ise = getEnv()
 	flag.Parse()
 	banner()
-	getNodes()
+
+	// generate urls for api calls and store them
+	initEndPoints()
+
+	// TODO generalize api call action
+	r := iseCall(endPoints["nodes"], nodelist)
+
+	// test output
+	fmt.Printf("type is %T\n", r)
+	fmt.Println(r.SearchResult.Resources[0].Name)
 }
 
 func init() {
@@ -60,10 +90,72 @@ func banner() {
 	return
 }
 
-// enumerate all nodes in the deployment and build iniital data structure
+//initalize endpoint url map
+func initEndPoints() {
+	endPoints["nodes"] = fmt.Sprintf("https://%s/ers/config/node", ise["pan"])
+	return
+}
+
+// generic function to make rest api call to ISE and pass the body back
+func iseCall(url string, nodelist NodeList) NodeList {
+
+	// make a transport
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true},
+	}
+
+	// make a client
+	client := &http.Client{Transport: tr}
+
+	// set up request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// build header
+	req.Header = http.Header{
+		"Content-Type": {"application/json"},
+		"Accept":       {"application/json"},
+	}
+
+	// add basic authentication to our header
+	req.SetBasicAuth(ise["user"], ise["password"])
+
+	// execute request & assign to res variable
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	// dump the header
+	fmt.Println(res)
+
+	//mdump the response body
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+
+	error := json.Unmarshal(b, &nodelist)
+	if error != nil {
+		log.Println(error)
+	}
+	//fmt.Println(j)
+	return nodelist
+
+	// fmt.Println(string(b))
+	// err := json.Unmarshal(b, &m)
+
+}
+
+// enumerate all nodes in the deployment and build inital data structure
 func getNodes() {
 	// build url
-	url := fmt.Sprintf("https://%s/ers/config/node", ise["pan"])
+	url := endPoints["nodes"]
 
 	// make a transport
 	tr := &http.Transport{
